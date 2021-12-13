e58863288332410152c36c1614d0f7cfb887e474
elsilv <elsi.silvennoinen@gmail.com>
2021-12-13 13:37:25 +0200

    Fix problem with cypress

--- bloglist-frontend/cypress/integration/blogs_app.js
+++ bloglist-frontend/cypress/integration/blogs_app.js
@@ -1,17 +1,17 @@
 describe('Blog app', function() {
     beforeEach(function() {
-      cy.request('POST', 'http://localhost:3000/api/testing/reset')
-      cy.request('POST', 'http://localhost:3000/api/users', {
+      cy.request('POST', 'http://localhost:3001/api/testing/reset')
+      cy.request('POST', 'http://localhost:3001/api/users', {
         username: 'mluukkai',
         name: 'Matti Luukkainen',
         password: 'salainen'
       })
-      cy.request('POST', 'http://localhost:3000/api/users', {
+      cy.request('POST', 'http://localhost:3001/api/users', {
         username: 'hellas',
         name: 'Arto Hellas',
         password: 'salainen'
       })
-      cy.visit('http://localhost:3000')
+      cy.visit('http://localhost:3001')
     })
   
     it('Login from is shown', function() {
